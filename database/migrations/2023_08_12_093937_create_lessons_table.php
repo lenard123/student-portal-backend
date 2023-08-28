a5f9ac63f50f406ddc777a99049cd82ceddcf34e
John Lenard Mangay-ayam <mangayayam.j@decodetech.ph>
2023-08-28 16:16:49 +0800

    fix

--- database/migrations/2023_08_12_093937_create_lessons_table.php
+++ database/migrations/2023_08_12_093937_create_lessons_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
             $table->foreignId('schedule_id')->constrained();
-            $table->foreignId('author_id')->constrained('user');
+            $table->foreignId('author_id')->constrained('users');
             $table->string('title');
             $table->text('description');
             $table->timestamps();
